2aa4b42c9f503b972e18cd5f5d5d94c01dcf36eb
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-07-29 11:58:30 -0400

    project euler even_fibonacci_sum

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def sum
+        fibonacci = [1, 2]
+        # Have to start my sum at 2 to catch the first even number in the initial fibonacci array
+        sum = 2
+        until fibonacci[-1] > self.limit 
+            fibonacci << fibonacci[-1] + fibonacci[-2]
+            if fibonacci[-1] % 2 == 0 && fibonacci[-1] < self.limit
+                sum = sum + fibonacci[-1]
+            end
+        end
+        sum
+    end
+end
